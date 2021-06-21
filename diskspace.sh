4f0c5869a7ea57ae3d34d65c30ce1ebc453e2121
anil93saini <66085686+anil93saini@users.noreply.github.com>
2021-06-21 15:06:30 +0530

    updated

--- diskspace.sh
+++ diskspace.sh
@@ -1,20 +1,16 @@
 #!/bin/bash  
-THRESHOLD=80 
+THRESHOLD=80
 MAILTO="anil93saini@gmail.com anil94saini@gamil.com"
 HOSTNAME=$(hostname)
-
-for diskN in `df -h | awk '{ print $5}' | sed 's/%//g'`
+for diskN in `df -h | grep -vE 'FileSystem|tmpfs' | awk '{ print $5}' | sed 's/%//g'`
 do
-	print $diskN
-	#CURRENT=$(df / | grep / | awk '{ print $6}' | sed 's/%//g')
 	 if [ $diskN -ge $THRESHOLD ] ; then  
 		df -h | grep $diskN% >> /tmp/temp 
 	 fi
 done
-
-VALUE = `cat /tmp/temp | wc -l`
+VALUE=`cat /tmp/temp | wc -l`
 if [ $VALUE -ge '1' ] ; then  
-	mail -s '${HOSTNAME}Disk Space Alert' anil93saini@gmail.com </tmp/temp
+	mail -s '${HOSTNAME}Disk Space Alert' $MAILTO </tmp/temp
 fi
 
 rm -rf /tmp/temp
